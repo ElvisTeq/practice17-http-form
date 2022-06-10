ace4dd5fb52511869df169878060bc8f2e92f46e
elvis <cenelvis@gmail.com>
2022-06-09 23:02:53 -0400

    Handling errors, try/catch, useState(err.message)

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,31 +6,42 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // Show loading (default true (because it is loading when loading page))
+  const [httpError, setHttpError] = useState();
 
   // useEffect() => cannot return a promise, but a function inside can "fetchMeals()"
   // Automatically fetch data at the end of event cycle
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch(
-        "https://react-http-58740-default-rtdb.firebaseio.com/meals.json"
-      );
-      const data = await response.json();
+      try {
+        const response = await fetch(
+          "https://react-http-58740-default-rtdb.firebaseio.com/meals.json"
+        );
 
-      const loadedMeals = [];
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
 
-      for (const key in data) {
-        // .push => Add {object} into [loadedMeals] array
-        loadedMeals.push({
-          // Data is stored as => {data.key.objects}
-          id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
-        });
+        const data = await response.json();
+
+        const loadedMeals = [];
+
+        for (const key in data) {
+          // .push => Add {object} into [loadedMeals] array
+          loadedMeals.push({
+            // Data is stored as => {data.key.objects}
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+
+        setMeals(loadedMeals);
+        setIsLoading(false); // Toggle loading message off
+      } catch (err) {
+        setIsLoading(false); // Toggle loading message off (because the one above will not run when "throw new Error")
+        setHttpError(err.message);
       }
-
-      setMeals(loadedMeals);
-      setIsLoading(false); // Toggle loading message off
     };
 
     fetchMeals();
@@ -39,6 +50,10 @@ const AvailableMeals = () => {
   // Show loading message if "fetchMeals()" is not finished running
   if (isLoading) {
     return <p className={classes.MealsLoading}>Loading...</p>;
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>{httpError}</section>;
   }
 
   const mealsList = meals.map((meal) => (
